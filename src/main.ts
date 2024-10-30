b1d8703dd13e1339bf6ea076a674e42c55350c89
Caio Alexandre de Andrade <116917490+caiodeandrade@users.noreply.github.com>
2024-10-30 00:11:44 -0300

    Update main.ts
    liberando cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  // Habilita o CORS para qualquer origem
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: false,
+  });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
